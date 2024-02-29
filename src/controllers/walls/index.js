78627c4b9064374b56d13ddb8f5736383efa449b
peibolcoru <pabloferreno.@gmail.com>
2024-02-29 10:15:38 +0100

    Cambio a index y corregir W de getwallsController

--- src/controllers/walls/index.js
+++ src/controllers/walls/index.js
@@ -1,4 +1,4 @@
-const getWallsController = require('./getwallsController');
+const getWallsController = require('./getWallsController');
 const newWallController = require ('./newWallController');
 const deleteWallController = require ('./deleteWallController')
 
